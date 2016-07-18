cf1bd9febaa2383e10b9476a5f3efa622ecb8d86
nguyenngoctam24 <nguyenngoctam24@gmail.com>
2016-07-18 17:57:25 +0700

    Update quickshorturl.php

--- quickshorturl.php
+++ quickshorturl.php
@@ -10,19 +10,18 @@ function curl($url)
 	return $source;
 	curl_close($ch);
 }
-
 /* Setup Ouo.io */
-$api['ouo.io'] = 'HwMoZSDE'; // <-- Replace with your API key
+$api['ouo.io'] = 'YOUR API KEY'; // <-- Replace with your API key
 /* 	How to:
 	Go to http://ouo.io/manage/tools/quick-link to get your API Key.
 	Ex: http://ouo.io/s/HwMoZSDE?s=yourdestinationlink.com.
 	API key is 'HwMoZSDE'.
-	Replace with your API.
+	Replace 'YOUR API KEY' with your API.
 */
 /* End Ouo.io */
 
 /* Setup shorte.st */
-$api['shorte.st'] = '6aeb2a82b22b41227d409d1985af366f';  // <-- Replace with your API key
+$api['shorte.st'] = 'YOUR API KEY';  // <-- Replace with your API key
 /* How to : 
 	Go to https://shorte.st/tools/quick to get your API Key.
 	Press Copy button to Copy your API key.
@@ -31,8 +30,8 @@ $api['shorte.st'] = '6aeb2a82b22b41227d409d1985af366f';  // <-- Replace with you
 /* End shorte.st */
 
 /* Setup adf.ly  */
-$uid['adf.ly'] = 14130025; // <-- Replace with your user ID
-$api['adf.ly'] = 'ea6fe35b7f1c3d1e08626ab76783bd7b'; // <-- Replace with your API key
+$uid['adf.ly'] = 'YOUR ID'; // <-- Replace with your user ID
+$api['adf.ly'] = 'YOUR API KEY'; // <-- Replace with your API key
 /* How to:
 	Go to https://adf.ly/publisher/tools#tools-api to get your public API key and your User ID
 	Replace 'YOUR ID' with your user ID and 'YOUR API KEY' with your public API;
@@ -42,12 +41,9 @@ $api['adf.ly'] = 'ea6fe35b7f1c3d1e08626ab76783bd7b'; // <-- Replace with your AP
 function short($a,$b)
 {	
 	global $api;
-	//var_dump($a);
 	if(isset($a['ouo']))
 	{	
 		$url = 'http://ouo.io/api/'.$api['ouo.io'].'?s='.$a['ouo'];
-		//var_dump($url);
-		//var_dump(curl($url));
 		if(!empty($b))
 		{	
 			return '<a href="'. curl($url) .'" '.'target="_blank">'. $b .'</a>';
@@ -59,8 +55,6 @@ function short($a,$b)
 	if(isset($a['shst']))
 	{	
 		$url = 'https://api.shorte.st/s/'.$api['shorte.st'].'/'.$a['shst'];
-		//var_dump($url);
-		//var_dump($a);
 		if(!empty($b))
 		{
 			return '<a href="'. json_decode(curl($url))->shortenedUrl .'" '. 'target="_blank">'. $b .'</a>';
